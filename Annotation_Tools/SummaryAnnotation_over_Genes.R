3aa96c52b481f13497f7119017df5fb77b4c67f7
raulmejia <r8a8m8p8@gmail.com>
2020-08-30 21:03:59 +0200

    from entrez ids adding summaries

--- Annotation_Tools/SummaryAnnotation_over_Genes.R
+++ Annotation_Tools/SummaryAnnotation_over_Genes.R
@@ -1,19 +1,26 @@
 #!/usr/bin/env Rscript
 #  www.github.com/raulmejia
-#  This file is free software: you may copy, redistribute and/or modify it  
-#  under the terms of the GNU General Public License as published by the  
-#  Free Software Foundation, either version 2 of the License, or (at your  
-#  option) any later version.  
-#  
-#  This file is distributed in the hope that it will be useful, but  
-#  WITHOUT ANY WARRANTY; without even the implied warranty of  
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU  
-#  General Public License for more details.  
-#  
-#  You should have received a copy of the GNU General Public License  
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>.  
+###################
+### Explanation ####
+####################
+# This script receives a file with Entrez ids as rows and retrieves a data frame including their summaries
+
+#########################
+#### Load libraries #####
+#########################
+if (!require("mygene")) {
+  BiocManager::install("mygene", dependencies = TRUE)
+  library("mygene")
+}
+
 suppressPackageStartupMessages(library("argparse"))
 
+
+
+
+############################## 
+## Data given by the user
+##############################
 # create parser object
 parser <- ArgumentParser()
 
@@ -24,14 +31,15 @@ parser$add_argument("-v", "--verbose", action="store_true", default=TRUE,
 parser$add_argument("-q", "--quietly", action="store_false", 
                     dest="verbose", help="Print little output")
 parser$add_argument("-i", "--inputfile", type="character", 
-                    help="file with your gene list in  Entrez or ensembl ids")
+                    help="input file with your gene list in  Entrez or ensembl ids")
 parser$add_argument("-o", "--outputfile", type="character", 
-                    help="file with your gene list in  Entrez or ensembl ids")
+                    help="output file where you want to store your results")
 # get command line options, if help option encountered print help and exit,
 # otherwise if options not found on command line then set defaults, 
 args <- parser$parse_args()
-
 # print some progress messages to stderr if "quietly" wasn't requested
+##################################
+
 
 myfile <-read.table(file=args$inputfile)
 #myfile <-read.table(file="/media/rmejia/mountme88/Projects/Phosoholipidosis/Documents/trashmegenes.txt")
@@ -40,7 +48,7 @@ myfile[1,1]
 str( myfile )
 list_queries <- list()
 for(k in 1:dim(myfile)[1]){
-  list_queries[k] <- getGene(myfile[k,1], fields="all")
+  list_queries[k] <- mygene::getGene(myfile[k,1], fields="all")
 }
 
 summaries <- list()
@@ -51,5 +59,5 @@ for(k in 1:length(list_queries)){ # making  a list that collect summarÿ́́s in
 retrivable <- data.frame( names(summaries) , as.character(summaries), stringsAsFactors = FALSE)
 
 
-write.table(file=args$inputfile ,retrivable, row.names = FALSE, sep="\t")
+write.table(file=args$outputfile ,retrivable, row.names = FALSE, sep="\t")
 
