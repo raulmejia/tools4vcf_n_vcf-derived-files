8e1e3dc09636137bc79a2ba297ac98fa616e6bbb
raulmejia <r8a8m8p8@gmail.com>
2021-04-08 13:26:31 +0200

    start filtering

--- filtering_vcf_derived_matrix.R
+++ filtering_vcf_derived_matrix.R
@@ -1,6 +1,6 @@
 ######################
 ### Filter me
-### This program receives a vcf convert its contents to a matrix and then organize some relevant information within it in a columnar way
+### This program receives a vcf and apply some filters defined by the user
 ######################
 ## then I can do a program that could receive different amount of parameters
 ## Always defined by a triplet parameter, operator, value.
@@ -18,17 +18,21 @@ if (!require("BiocManager")) {
   install.packages("BiocManager", ask =FALSE)
   library("BiocManager")
 }
+if (!require("limma")) {
+  BiocManager::install("limma", ask =FALSE)
+  library("limma")
+}
 if (!require("tidyverse")) {
   install.packages("tidyverse", ask =FALSE)
   library("tidyverse")
 }
-if (!require("vcfR")) {
-  install.packages("vcfR", ask =FALSE)
-  library("vcfR")
-}
 if (!require("stringr")) {
   install.packages("stringr", ask =FALSE)
   library("stringr")
+}
+if (!require("ggplot2")) {
+  install.packages("ggplot2", ask =FALSE)
+  library("ggplot2")
 }
 
 ##########################
@@ -36,17 +40,17 @@ if (!require("stringr")) {
 ##########################
 myargs <- commandArgs(trailingOnly = TRUE)
 
-# path_your_vcf <-"/media/rmejia/mountme88/Projects/Phosoholipidosis/Exome_Lipidosis/VCFs_Backup_to_work/Exact-Positions-match_DIL1_and_DIL2_and_Mom_A-FDad-as-bkgd.vcf"
-path_your_vcf <- myargs[1]
+# path_your_table <-"/media/rmejia/mountme88/Projects/Phosoholipidosis/Exome_Lipidosis/VCFs_Backup_to_work/Vcfderived-matrices_columnized-info/Exact-Positions-match_DIL1_and_DIL2_and_Mom_A-FDad-as-bkgd.vcf_table_derived_and_ExAC-g1000-esp5400-clinvar-type-NCh-PCh-dbSNP-Poly-Gen-columnized.tsv"
+path_your_table <- myargs[1]
 
 # code_path <- "/media/rmejia/mountme88/code/tools4vcf_n_vcf-derived-files/"
 code_path <- myargs[2]
 
-# Folder_to_save_Results <-"/media/rmejia/mountme88/Projects/Phosoholipidosis/Exome_Lipidosis/VCFs_Backup_to_work/Vcfderived-matrices_columnized-info"
+# Folder_to_save_Results <-"/media/rmejia/mountme88/Projects/Phosoholipidosis/Exome_Lipidosis/VCFs_Backup_to_work/Vcfderived-matrices_filtered"
 Folder_to_save_Results <- myargs[3]
 
 #your_label <- "Diagnoses"
-#your_x_label <- myargs[4]
+#your_x_label <- myargs[8]
 
 #######################
 ### Body
@@ -57,11 +61,14 @@ Folder_to_save_Results <- normalizePath( Folder_to_save_Results)
 code_path <- normalizePath( code_path)
 source(paste0(code_path,"/libraries/functions_to_columnize_vcf_derived_tables.R"))
 
-# reading your vcf
-myvcf <- read.vcfR(path_your_vcf)
+# reading your v
+mytable <- read.table( path_your_matrix , header = TRUE)
+
+myCPRAtable <- read.table("/media/rmejia/mountme88/Projects/Phosoholipidosis/Exome_Lipidosis/VCFs_Backup_to_work/vcfs_as_matrices/Exact-Positions-match_DIL1_and_DIL2_and_Mom_A-FDad-as-bkgd.vcf_GTinfoDeleted__CPRAadded.tsv", header = TRUE)
+#head(myCPRAtable)
 
 # Extracting the info and put it in a columnar way 
-mytable_ExAC_columnized <- vcfderived_table_columnizing_ExAC( myvcf@fix )
+mytable_ExAC_columnized <- vcfderived_table_columnizing_ExAC( myCPRAtable )
 mytable_g1000_columnized <- vcfderived_table_columnizing_g1000 (mytable_ExAC_columnized )
 mytable_esp5400_columnized <- vcfderived_table_columnizing_esp5400(mytable_g1000_columnized )
 mytable_type_of_var_columnized <- vcfderived_table_columnizing_type_of_var_by_transcription(   mytable_esp5400_columnized )
@@ -75,8 +82,6 @@ mytable_GeneName_columnized <- vcfderived_table_columnizing_GeneName( mytable_Po
 head( mytable_GeneName_columnized , n = 50 )
 #dim(mytable_GeneName_columnized)
 
-
 ### save it
 path_2_savefile <- paste0( Folder_to_save_Results ,"/",basename( path_your_vcf ),"_table_derived_and_ExAC-g1000-esp5400-clinvar-type-NCh-PCh-dbSNP-Poly-Gen-columnized.tsv" )
-write.table( mytable_GeneName_columnized , path_2_savefile , sep ="\t", row.names = FALSE)
-
+write.table( mytable_GeneName_columnized , path_2_savefile , sep ="\t")
--- vcfderivedmatrix_columnizing-relevant-values.R
+++ vcfderivedmatrix_columnizing-relevant-values.R
@@ -1,12 +1,10 @@
 ######################
 ### Filter me
-### This program receives a vcf convert its contents to a matrix and then organize some relevant information within it in a columnar way
+### This program receives a vcf-derived matrix and convert its contents to a matrix and then organize some relevant information within it in a columnar way
 ######################
 ## then I can do a program that could receive different amount of parameters
 ## Always defined by a triplet parameter, operator, value.
 ## 
-# Name of the Label in your vcf and 
-# parameter to choose in label vcf 
 ##########################
 ### Installing and or Loading the required packages 
 ##########################
@@ -22,10 +20,6 @@ if (!require("tidyverse")) {
   install.packages("tidyverse", ask =FALSE)
   library("tidyverse")
 }
-if (!require("vcfR")) {
-  install.packages("vcfR", ask =FALSE)
-  library("vcfR")
-}
 if (!require("stringr")) {
   install.packages("stringr", ask =FALSE)
   library("stringr")
@@ -36,8 +30,8 @@ if (!require("stringr")) {
 ##########################
 myargs <- commandArgs(trailingOnly = TRUE)
 
-# path_your_vcf <-"/media/rmejia/mountme88/Projects/Phosoholipidosis/Exome_Lipidosis/VCFs_Backup_to_work/Exact-Positions-match_DIL1_and_DIL2_and_Mom_A-FDad-as-bkgd.vcf"
-path_your_vcf <- myargs[1]
+# path_your_vcf_derived_matrix <-"/media/rmejia/mountme88/Projects/Phosoholipidosis/Exome_Lipidosis/VCFs_Backup_to_work/Exact-Positions-match_DIL1_and_DIL2_and_Mom_A-FDad-as-bkgd.vcf"
+path_your_vcf_derived_matrix <- myargs[1]
 
 # code_path <- "/media/rmejia/mountme88/code/tools4vcf_n_vcf-derived-files/"
 code_path <- myargs[2]
@@ -46,7 +40,7 @@ code_path <- myargs[2]
 Folder_to_save_Results <- myargs[3]
 
 #your_label <- "Diagnoses"
-#your_x_label <- myargs[4]
+#your_x_label <- myargs[8]
 
 #######################
 ### Body
@@ -58,10 +52,13 @@ code_path <- normalizePath( code_path)
 source(paste0(code_path,"/libraries/functions_to_columnize_vcf_derived_tables.R"))
 
 # reading your vcf
-myvcf <- read.vcfR(path_your_vcf)
+# myvcf <- read.vcfR(path_your_vcf)
+
+myCPRAtable <- read.table(path_your_vcf_derived_matrix, header = TRUE)
+#head(myCPRAtable)
 
 # Extracting the info and put it in a columnar way 
-mytable_ExAC_columnized <- vcfderived_table_columnizing_ExAC( myvcf@fix )
+mytable_ExAC_columnized <- vcfderived_table_columnizing_ExAC( myCPRAtable )
 mytable_g1000_columnized <- vcfderived_table_columnizing_g1000 (mytable_ExAC_columnized )
 mytable_esp5400_columnized <- vcfderived_table_columnizing_esp5400(mytable_g1000_columnized )
 mytable_type_of_var_columnized <- vcfderived_table_columnizing_type_of_var_by_transcription(   mytable_esp5400_columnized )
@@ -72,11 +69,9 @@ mytable_dbSNP_columnized <- vcfderived_table_columnizing_dbSNP(mytable_Protein_c
 mytable_Polyphen2_columnized <- vcfderived_table_columnizing_Polyphen2( mytable_dbSNP_columnized )
 mytable_GeneName_columnized <- vcfderived_table_columnizing_GeneName( mytable_Polyphen2_columnized  )
 
-head( mytable_GeneName_columnized , n = 50 )
-#dim(mytable_GeneName_columnized)
-
+# head( mytable_GeneName_columnized , n = 50 )
+# dim(mytable_GeneName_columnized)
 
 ### save it
-path_2_savefile <- paste0( Folder_to_save_Results ,"/",basename( path_your_vcf ),"_table_derived_and_ExAC-g1000-esp5400-clinvar-type-NCh-PCh-dbSNP-Poly-Gen-columnized.tsv" )
+path_2_savefile <- paste0( Folder_to_save_Results ,"/",basename( path_your_vcf_derived_matrix),"_table_derived_and_ExAC-g1000-esp5400-clinvar-type-NCh-PCh-dbSNP-Poly-Gen-columnized.tsv" )
 write.table( mytable_GeneName_columnized , path_2_savefile , sep ="\t", row.names = FALSE)
-
